acf523177cdadffb09e34ab0d2f0454c12fdc6db
William Zhou <williamliangzhou2005@gmail.com>
2025-02-16 14:40:05 -0500

    create ID for bullet, to make sure same bullet don't hit same multiple times

--- src/Bullet/Bullet.h
+++ src/Bullet/Bullet.h
@@ -6,27 +6,29 @@
 
 class Bullet {
     private:
+        int id;
         Vector2 position;
         float damage, speed;
         Vector2 direction;
         Texture *texture;
 
+        static int idCounter;
+
     public:
         Bullet(Vector2 position, float damage, float speed, Vector2 direction, Texture* texture);
         Bullet();
 
-        Vector2 getPosition() const;
-        float getDamage() const;
-        float getSpeed() const;
-        Vector2 getDirection() const;
-        Texture* getTexture() const;
+        Vector2 getPosition() const { return position; };
+        float getDamage() const { return damage; };
+        float getSpeed() const { return speed; };
+        Vector2 getDirection() const { return direction; };
+        Texture* getTexture() const { return texture; };
 
-        void setPosition(Vector2 position);
-        void setDamage(float damage);
-        void setSpeed(float speed);
-        void setDirection(Vector2 direction);
-        void setTexture(Texture* texture);
-
+        void setPosition(Vector2 position){ this->position = position; }
+        void setDamage(float damage){ this->damage = damage; }
+        void setSpeed(float speed){ this->speed = speed; }
+        void setDirection(Vector2 direction){ this->direction = direction; }
+        void setTexture(Texture* texture){ this->texture = texture; }
 
         void render();
         void update(float deltaTime);
